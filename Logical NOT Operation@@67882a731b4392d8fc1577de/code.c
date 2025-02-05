6f352c141fa291fe8a27023b329ef48726c1dbc8
sakshi26042007 <26sakshisingla@gmail.com>
2025-02-05 02:12:05 -0300

    Solution to Logical NOT Operation

---
+++ Logical NOT Operation@@67882a731b4392d8fc1577de/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num1;
+    scanf("%d", &num1);
+    if (!(num1 > 0)) {
+        printf("True");
+    } else {
+        printf("False");
+    }
+
+    return 0;
+}
